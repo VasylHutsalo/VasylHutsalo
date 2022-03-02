8f8f42a38fac68113a8554fe0d3d61fd03a89726
Vasyl Hutsalo <gvl59ma@gmail.com>
2022-03-02 23:29:43 +0300

    Домашнее задание №1

---
+++ Task1/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача № 1
+/*
+Console.WriteLine("Введите первое число: ");
+int value1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int value2 = Convert.ToInt32(Console.ReadLine());
+
+if(value1 > value2)
+{
+    Console.Write("max: ");
+    Console.WriteLine(value1);
+}
+else
+{
+     Console.Write("max: ");
+Console.WriteLine(value2);
+}
+*/
+
+// Задача №2
+/*
+Console.WriteLine("Введите первое число: ");
+int value1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int value2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+int value3 = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+if(value1 > max) max = value1;
+if(value2 > max) max = value2;
+if(value3 > max) max = value3;
+Console.Write("max: ");
+Console.WriteLine(max);
+*/
+
+// Задача №3
+/*
+Console.WriteLine("Введите число: ");
+int value = Convert.ToInt32(Console.ReadLine());
+
+if(value % 2 == 0)
+{
+Console.Write(" Четное: ");
+Console.WriteLine(value);
+}
+else
+{
+Console.Write("Нечетное: ");
+Console.WriteLine(value);
+}
+*/
+
+//Задача №4
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+while (a <= N)
+{
+    if(a % 2 == 0)
+   {
+    Console.WriteLine(a);
+    a++; 
+   }  
+}
+       
+    
+
+
